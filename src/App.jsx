ec52134c95736bbd64d6618836e86454a1552f64
Sevenwings26 <iarowosola@yahoo.com>
2024-06-03 17:00:49 +0100

    Update Learning

--- src/App.jsx
+++ src/App.jsx
@@ -1,91 +1,48 @@
 import React from "react";
-import Todo from './components/Todo.jsx'
+import Todo from "./components/Todo.jsx";
 import Form from "./components/Form.jsx";
 import FilterButton from "./components/FilterButton.jsx";
+import { useState } from "react";
+import { nanoid } from "nanoid"
 
+// Using Functional State
 function App(props) {
-  // to avoid repitition - variable to be called later 
-  const taskList = props.tasks?.map((task) => (
-  <Todo 
-  id={task.id} 
-  name={task.name} 
-  completed = {task.completed} 
-  key = {task.id} 
-  />
+  const [tasks, setTasks] = useState(props.tasks);
+
+  function addTask(name) {
+    const newTask = { id: `todo-${nanoid()}`, name, completed: false };
+    setTasks([...tasks, newTask]);
+  }
+
+  // Count the heading 
+  const tasksNoun = taskList.length !== 1 ? "tasks" : "task";
+  const headingText = `${taskList.length} ${tasksNoun} remaining`;
+
+  // to avoid repitition - variable to be called later
+  const taskList = tasks?.map((task) => (
+    <Todo
+      id={task.id}
+      name={task.name}
+      completed={task.completed}
+      key={task.id}
+    />
   ));
   return (
     <div className="todoapp stack-large">
       <h1>TodoMatic</h1>
-      <Form />
+      <Form addTask={addTask} />
       <FilterButton />
       <FilterButton />
       <FilterButton />
 
-      {/* <div className="filters btn-group stack-exception">
-        <button type="button" className="btn toggle-btn" aria-pressed="true">
-          <span className="visually-hidden">Show </span>
-          <span>All</span>
-          <span className="visually-hidden"> tasks</span>
-        </button>
-        <button type="button" className="btn toggle-btn" aria-pressed="true">
-          <span className="visually-hidden">Show</span>
-          <span> Active </span>
-          <span className="visually-hidden">tasks</span>
-        </button>
-        <button type="button" className="btn toggle-btn" aria-pressed="false">
-          <span className="visually-hidden">Show </span>
-          <span>Completed</span>
-          <span className="visually-hidden"> tasks</span>
-        </button>
-      </div> */}
-
-      <h2 id="list-heading">3 tasks remaining</h2>
-      
+      <h2 id="list-heading">{headingText}</h2>
       <ul
         role="list"
         className="todo-list stack-large stack-exception"
         aria-labelledby="list-heading"
       >
-      {taskList}
-
-      {/* Repititive  */}
-      {/* <Todo name='Eat' id="todo-0" completed />
-      <Todo name='Code' id="todo-1"/>
-      <Todo name='Rehearse' id="todo-2"/> */}
-{/*         
-        <li className="todo stack-small">
-          <div className="c-cb">
-            <input id="todo-1" type="checkbox" />
-            <label className="todo-label" htmlFor="todo-1">
-              Sleep
-            </label>
-          </div>
-          <div className="btn-group">
-            <button type="button" className="btn">
-              Edit <span className="visually-hidden">Sleep</span>
-            </button>
-            <button type="button" className="btn btn__danger">
-              Delete <span className="visually-hidden">Sleep</span>
-            </button>
-          </div>
-        </li>
-
-        <li className="todo stack-small">
-          <div className="c-cb">
-            <input id="todo-2" type="checkbox" />
-            <label className="todo-label" htmlFor="todo-2">
-              Repeat
-            </label>
-          </div>
-          <div className="btn-group">
-            <button type="button" className="btn">
-              Edit <span className="visually-hidden">Repeat</span>
-            </button>
-            <button type="button" className="btn btn__danger">
-              Delete <span className="visually-hidden">Repeat</span>
-            </button>
-          </div>
-        </li> */}
+        {taskList}
+        {/* Repititive  */}
       </ul>
     </div>
   );
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,18 +1,32 @@
-function Form() {
+import { useState } from "react";
+
+function Form(props) {
+  // set State for changing events
+  const [name, setName ] = useState("");
+
+  // handle Change 
+  function handleChange(event) {
+    setName(event.target.value);
+  }
+  
+  // Using event 
+  function handleSubmit(event) {
+    event.preventDefault();
+    props.addTask(name);
+    setName("");
+  }
+
   return (
     <div>
-      <form>
+      <form onSubmit={handleSubmit} >
         <h2 className="label-wrapper">
           <label htmlFor="new-todo-input" className="label_lg">
             What needs to be done?
           </label>
         </h2>
-        <input
-          type="text"
-          id="new-todo-input"
-          className="input input-lg"
-          name="text"
-          autoComplete="off"
+        <input type="text"
+          id="new-todo-input" className="input input-lg" name="text" autoComplete="off" 
+          value={name} onChange={handleChange}
         />
         <button type="submit" className="btn btn__primary btn__lg">
           Add
